14376ea0683b18bef0fe66d593430a56f0c50488
g-WSL <gsakore22@gmail.com>
2025-08-23 22:16:39 +0530

    seed file update push backlog

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+
+UserBlock::Admin.create(name: "admin", email: "admin@ezallocations.com", password: "Admin@123", mobile_number: "1234567891", status:"active", verified: true) if UserBlock::Admin.count <= 0
+
+puts 'Creating sample financial entities - '
+
+# Create a sample financial entity if none exist
 if FinancialEntity.count <= 0 then
   puts 'Creating sample fianancial entity - '
   fe = FinancialEntity.create(name: 'bank1', code: 'ABCD')
